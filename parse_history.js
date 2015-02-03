8052d2f9b461fdd841f18f8d92288bd4c8cf9ad0
Shing Lyu <shing.lyu@gmail.com>
2015-02-03 18:19:33 +0800

    Added corrected links

--- parse_history.js
+++ parse_history.js
@@ -1,5 +1,7 @@
 var https = require('https');
+var fs = require('fs');
 var url = "https://bugzilla.mozilla.org/rest/bug/1023688/history";
+var outfile = "./demodata_history.json"
 
 function parseHistory(response) {
   var histories = response.bugs[0].history;
@@ -12,42 +14,101 @@ function parseHistory(response) {
       }
     })
   })
-  console.log(fieldnames)
+  //console.log(fieldnames)
 
-  var changes = {};
+  var changes = [];
+
+  var id = 0
   histories.map(function(h){
-    if (h.when in changes) {
-      h.changes.map(function(c){
-        changes[h.when][c.field_name].push(c);
-        /*
-        if (c.field_name in changes[h.when]) {
-          changes[h.when][c.field_name].push(c);
+    h.changes.map(function(c){
+      c.added.split(',').map(function(a){
+        if (a == "") {
+          return
         }
-        else {
-          changes[h.when][c.field_name] = [c];
+        var node = {}
+        node.name = a.trim()
+        node.type = c.field_name;
+        node.action= "+";
+        node.id = id;
+        id += 1;
+        node.date = h.when
+        node.from = "R"
+        changes.push(node)
+      });
+      c.removed.split(',').map(function(a){
+        if (a == "") {
+          return
         }
-        */
-      })
-    }
-    else {
-      var entry = {when: h.when};
-      fieldnames.map(function(f){entry[f] = []})
-      changes[h.when] = entry
-
-      h.changes.map(function(c){
-        changes[h.when][c.field_name].push(c);
-      })
-    }
+        var node = {}
+        node.name = a.trim()
+        node.type = c.field_name;
+        node.action = "-";
+        node.id = id;
+        id += 1;
+        node.date = h.when
+        node.from = "R"
+        changes.push(node)
+      });
+    })
   })
 
+
+  links = generateLinks(changes);
+  parsed = {"nodes": changes, "links": links}
+  console.log(JSON.stringify(parsed, null, 2))
+  fs.writeFile(outfile, JSON.stringify(parsed, null, 2))
+
+  /*
   for (var time in changes) {
     var arr = Object.keys(changes[time]).map(function(key) {return changes[time][key]})
     var msg = arr.join('\t')
     console.log(msg);
 
   }
+  */
 }
 
+function generateLinks(changes){
+  stack = {}
+  changes.map(function(node){
+
+    if (!stack.hasOwnProperty(node.type)) {
+      stack[node.type] = {}
+    }
+    //else {
+    //console.log(node)
+    if (!stack[node.type].hasOwnProperty(node.name)) {
+      //console.log("created new")
+      stack[node.type][node.name] = [node]
+    }
+    else {
+      //console.log("pushed")
+      stack[node.type][node.name].push(node);
+    }
+    //}
+  })
+
+  //console.log(stack)
+  links = []
+  for (var type in stack) {
+    for (var name in stack[type]){
+      //console.log('---')
+      if (stack[type][name].length > 1) {
+        nodes = stack[type][name]
+        for (var i = 1; i < nodes.length; i++) {
+          //console.log(nodes[i-1])
+          links.push({"source": nodes[i-1].id, "target": nodes[i].id, "type": "answeredby"})
+          
+        }
+        //console.log(nodes[nodes.length-1])
+      }
+    }
+  }
+  
+  return links
+}
+
+/*
 https.get(url, function(res) {
   var body = '';
 
@@ -57,10 +118,17 @@ https.get(url, function(res) {
 
   res.on('end', function() {
     var response = JSON.parse(body);
-    console.log("Got response: ", response);
+    //console.log("Got response: ", response);
     parseHistory(response);
   });
 
 }).on('error', function(e) {
-  console.log("Got error: ", e);
+  //console.log("Got error: ", e);
 });
+*/
+
+fs.readFile('../test/mock_history.json', function(err, data){
+  if (err) throw err;
+  //console.log(data)
+  parseHistory(JSON.parse(data))
+})
